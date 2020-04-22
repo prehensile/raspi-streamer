7815482aa62038f9ab2864444cc71829cb2c40a8
Henry Cooke <me@prehensile.co.uk>
2020-04-22 16:15:40 +0100

    new source for widevine plugin

--- stage-2.sh
+++ stage-2.sh
@@ -9,11 +9,11 @@ chmod +x ~/$LAUNCHER_SCRIPT
 
 sudo -s <<HEREDOC
 # install widevine plugin
-# see https://blog.vpetkov.net/2019/07/12/netflix-and-spotify-on-a-raspberry-pi-4-with-latest-default-chromium/
-cd /usr/lib/chromium-browser
-wget http://blog.vpetkov.net/wp-content/uploads/2019/09/libwidevinecdm.so_.zip
-unzip libwidevinecdm.so_.zip && chmod 755 libwidevinecdm.so
-rm libwidevinecdm.so_.zip
+# see https://blog.vpetkov.net/2020/03/30/raspberry-pi-netflix-one-line-easy-install-along-with-hulu-amazon-prime-disney-plus-hbo-spotify-pandora-and-many-others/
+cd /tmp
+wget -q --no-check-certificate https://pi.vpetkov.net/libwidevinecdm.so_.zip
+unzip -q libwidevinecdm.so_.zip && chmod 755 libwidevinecdm.so && rm -f libwidevinecdm.so_.zip
+mv -f libwidevinecdm.so /usr/lib/chromium-browser
 
 # install shairport
 sudo apt install shairport-sync
